b453f62b75d1263efe2abc071e04507a2dce2751
Dustin Guichet <dustin.guichet@protonmail.com>
2021-07-03 23:48:28 -0500

    Finished document

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,7 +2,7 @@
 function myfunc() {
 
 // Setting DOM To All Boxes Or Input Fields
-    var b1, b1, b3, b4, b5, b6, b7, b8, b9;
+    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
     b3 = document.getElementById("b3").value;
