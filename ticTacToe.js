60b57e928fb720727f041d20fde2f99a26765352
Dustin Guichet <dustin.guichet@protonmail.com>
2021-07-03 23:24:04 -0500

    Reset Function Added

--- ticTacToe.js
+++ ticTacToe.js
@@ -273,7 +273,20 @@ function myfunc() {
             document.getElementById('print')
                 .innerHTML = "Player 0 Turn";
         }
-   }
+    }
 }
 
 // Function To Reset Game
+
+function myfunc_2() {
+    location.reload();
+    document.getElementById('b1').value = '';
+    document.getElementById("b2").value = '';
+    document.getElementById("b3").value = '';
+    document.getElementById("b4").value = '';
+    document.getElementById("b5").value = '';
+    document.getElementById("b6").value = '';
+    document.getElementById("b7").value = '';
+    document.getElementById("b8").value = '';
+    document.getElementById("b9").value = '';
+}
